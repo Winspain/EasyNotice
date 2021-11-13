db4dc5bba82c8dda8cc916d944f9afcff713cacd
winspain <919192088@qq.com>
2021-11-13 16:39:51 +0800

    feat:去除local文件

--- Notice/easy_notice/settings/local.py
+++ Notice/easy_notice/settings/local.py
@@ -8,14 +8,3 @@ from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '47.96.117.252',
-        'NAME': 'easy_notice',
-        'USER': 'root',
-        'PASSWORD': 'atec@123',
-        'OPTIONS': {'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'}
-    }
-}
